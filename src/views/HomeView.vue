31ae01786176eda899d91899e878d750293c0651
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2023-08-24 18:02:03 +0900

    el-table出たよ

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-table
+  :data="tableData"
+  stripe
+  style="width: 100%">
+  <el-table-column
+    prop="date"
+    label="Date"
+    width="180">
+  </el-table-column>
+  <el-table-column
+    prop="name"
+    label="Name"
+    width="180">
+  </el-table-column>
+  <el-table-column
+    prop="address"
+    label="Address">
+  </el-table-column>
+</el-table>
+<!-- <div class="home">
+  <img alt="Vue logo" src="../assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</div> -->
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      tableData: [{
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-04',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }]
+    }
   }
 }
+// // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+// export default {
+//   name: 'HomeView',
+//   components: {
+//     HelloWorld
+//   }
+// }
 </script>
